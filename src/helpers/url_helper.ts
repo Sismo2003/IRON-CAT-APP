ea3360d5bba24cd9cf2cae62fff6996e477abab4
Giovanni Escoto <a22110321@ceti.mx>
2025-03-18 14:41:33 -0600

    Feat: Images avatares

--- src/helpers/url_helper.ts
+++ src/helpers/url_helper.ts
@@ -1,5 +1,5 @@
-// const servidor_ip : string = "http://thegrid.myddns.me:3000";
-const servidor_ip : string = "http://localhost:3000"; // TEST
+const servidor_ip : string = "http://thegrid.myddns.me:3000";
+// const servidor_ip : string = "http://localhost:3000"; // TEST
 
 // REGISTER
 export const POST_FAKE_REGISTER = "/auth/signup";
