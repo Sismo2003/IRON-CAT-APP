c3edc7a3b0b5bc71f5d380ef094b7308acdf6ccc
Giovanni Escoto <a22110321@ceti.mx>
2025-04-04 10:48:03 -0600

    feat:Translation changes

--- src/pages/Ecommerce-Sales/Products/ListView.tsx
+++ src/pages/Ecommerce-Sales/Products/ListView.tsx
@@ -175,7 +175,7 @@ const ListView = () => {
                     <div className="grid grid-cols-1 gap-4 lg:grid-cols-1 xl:grid-cols-12">
                         <div className="xl:col-span-3">
                             <div className="relative">
-                                <input type="text" className="ltr:pl-8 rtl:pr-8 search form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Search for ..." autoComplete="off" onChange={(e) => filterSearchData(e)} />
+                                <input type="text" className="ltr:pl-8 rtl:pr-8 search form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Buscar" autoComplete="off" onChange={(e) => filterSearchData(e)} />
                                 <Search className="inline-block size-4 absolute ltr:left-2.5 rtl:right-2.5 top-2.5 text-slate-500 dark:text-zink-200 fill-slate-100 dark:fill-zink-600" />
                             </div>
                         </div>
